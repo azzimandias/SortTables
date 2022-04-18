a8242a3018bbc4f3c497f215ab1ef43e2ef8b9cd
Andrew Sharanda <azzimandias@gmail.com>
2022-04-18 13:42:01 +0300

    Add normalize.css

--- Express.js
+++ Express.js
@@ -14,5 +14,5 @@ app.listen(port, (err) => {
     if (err) {
         return console.log('something bad happened', err)
     }
-    console.log(`server is listening on ${port}`)
+    console.log(` server is listening on ${port}\n http://localhost:3000/`)
 });
