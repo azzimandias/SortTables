0d377010a540d4bcbc3cb2fbd7cd5f0593658835
Andrew Sharanda <azzimandias@gmail.com>
2022-04-18 18:22:20 +0300

    Fix directory

--- src/express.js
+++ src/express.js
@@ -2,10 +2,10 @@ const express = require('express');
 const app = express();
 const port = 3000;
 app.set('view engine', 'html');
-app.use(express.static(__dirname));
+app.use(express.static(__dirname.split('src')[0]));
 
 app.get(`/`, (request, response) => {
-    response.sendFile(__dirname + '/index.html', err => {
+    response.sendFile(__dirname.split('src')[0] + 'public/index.html', err => {
         console.log('something bad happened', err);
     });
 });
